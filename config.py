99b86f3f56f2492e01369ef40e0610ccbe33ee8a
Turbo-KROT <smart.drone@bk.ru>
2026-09-16 10:04:39 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Токен твоего бота из @BotFather
+BOT_TOKEN = "8952916258:AAFcVz56klmE7o95kxoOAKLM3UxAjMPk_bE"
+
+# Учётные данные GigaChat
+# Получить: https://developers.sber.ru/studio/workspaces
+GIGACHAT_CREDENTIALS = "MDFhMGE5MDMtMGI0Yi03NjVjLTkyODctYzQwMzc4MmY4MTliOmQ4NWVmZjdkLWM3NjYtNGJkZS1hNmIyLTc1YWQ1Y2Y2MmYzYw=="
+
+# Твой Telegram ID (чтобы получать уведомления)
+# Как узнать: напиши боту @userinfobot — он пришлёт твой ID
+ADMIN_ID = 880354478
